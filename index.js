e6abdbbd879408f455d7d8034d91bbfc0887f4dc
Tara E. Sabella <tarasabella@gmail.com>
2018-09-11 16:39:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = [];
 function takeANumber(line, name) {
   line.push('${name}');
-  return ('Welcome, ${'name'}. You are number line.length in line.');
+  return ('Welcome, ${name}. You are number line.length in line.');
 }
