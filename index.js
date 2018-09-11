0a4fa6a6085b5a41a5f60c0324d7a69d8d5699bd
Tara E. Sabella <tarasabella@gmail.com>
2018-09-11 16:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ function takeANumber(line, name) {
   line.push('${name}');
   return ('Welcome, ${name}. You are number ${line.length} in line.');
 }
+function takeANumber(katzDeliLine, nowServing) {
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+ 
+ var line = [];
+ function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i]);
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
